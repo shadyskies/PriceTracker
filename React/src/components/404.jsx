12d37aed90bd301f38273218e73060a1a8b09aa4
therajtiwari <therajtiwari254@gmail.com>
2021-01-09 19:46:06 +0530

    ui (login and tentative home page)

---
+++ React/src/components/404.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Error404() {
+  const style = {
+    backgroundColor: "black",
+    // background: "transparent",
+    color: "white",
+
+    fontSize: "3rem",
+    // textAlign: "center",
+    width: "100%",
+    padding: "1rem",
+  };
+  return <h1 style={style}>Page not found</h1>;
+}
+
+export default Error404;
