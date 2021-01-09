b4bffe8608a424f89cea2cc530d07af3aacad942
therajtiwari <therajtiwari254@gmail.com>
2021-01-09 22:17:36 +0530

    added price comparison by fetching data from json

--- React/src/components/home.jsx
+++ React/src/components/home.jsx
@@ -1,101 +1,111 @@
-import { Navbar, Button, Nav, Form, FormControl } from "react-bootstrap";
+/* App.js */
+import { Navbar, Button, Nav, Form } from "react-bootstrap";
+// import { useState } from "react";
 var React = require("react");
 var Component = React.Component;
 var CanvasJSReact = require("./canvasjs.react").default;
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-function Home() {
-  const options = {
-    animationEnabled: true,
-    title: {
-      text: "Price Comparison",
-    },
-    axisY: {
-      title: "Number of Customers",
-    },
-    toolTip: {
-      shared: true,
-    },
-    data: [
-      {
-        type: "spline",
-        name: "Amazon",
-        showInLegend: true,
-        dataPoints: [
-          { y: 155, label: "Jan" },
-          { y: 150, label: "Feb" },
-          { y: 152, label: "Mar" },
-          { y: 148, label: "Apr" },
-          { y: 142, label: "May" },
-          { y: 150, label: "Jun" },
-          { y: 146, label: "Jul" },
-          { y: 149, label: "Aug" },
-          { y: 153, label: "Sept" },
-          { y: 158, label: "Oct" },
-          { y: 154, label: "Nov" },
-          { y: 150, label: "Dec" },
-        ],
+var datapoint_amazon = [];
+var datapoint_flipkart = [];
+// var [datapoint_amazon, setdatapoint_amazon] = useState([]);
+// var [datapoint_flipkart,setdatapoint_flipkart]=useState([]);
+
+class Home extends Component {
+  render() {
+    const options = {
+      animationEnabled: true,
+      title: {
+        text: "Price Comparison",
       },
-      {
-        type: "spline",
-        name: "Flipkart",
-        showInLegend: true,
-        dataPoints: [
-          { y: 172, label: "Jan" },
-          { y: 173, label: "Feb" },
-          { y: 175, label: "Mar" },
-          { y: 172, label: "Apr" },
-          { y: 162, label: "May" },
-          { y: 165, label: "Jun" },
-          { y: 172, label: "Jul" },
-          { y: 168, label: "Aug" },
-          { y: 175, label: "Sept" },
-          { y: 170, label: "Oct" },
-          { y: 165, label: "Nov" },
-          { y: 169, label: "Dec" },
-        ],
+      axisY: {
+        title: "Price in Rs",
       },
-    ],
-  };
+      toolTip: {
+        shared: true,
+      },
+      data: [
+        {
+          name: "Amazon",
+          showInLegend: true,
+          type: "spline",
+          dataPoints: datapoint_amazon,
+        },
+        {
+          type: "spline",
+          name: "Flipkart",
+          showInLegend: true,
+          dataPoints: datapoint_flipkart,
+        },
+      ],
+    };
+    return (
+      <div className="home-page">
+        <Navbar className="color-nav" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">About</Nav.Link>
+          </Nav>
+        </Navbar>
+        <br />
 
-  return (
-    <div className="home-page">
-      <Navbar className="color-nav" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">About</Nav.Link>
-        </Nav>
-      </Navbar>
-      <br />
+        <div className="product-form">
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Enter the Amazon URL of product</Form.Label>
+              <Form.Control placeholder="Amazon URL" />
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Enter the Flipkart URL of product</Form.Label>
+              <Form.Control placeholder="Flipkart URL" />
+            </Form.Group>
 
-      <div className="product-form">
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Enter the amazon URL of product</Form.Label>
-            <Form.Control placeholder="Amazon URL" />
-          </Form.Group>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Enter the Flipkart URL of product</Form.Label>
-            <Form.Control placeholder="Flipkart URL" />
-          </Form.Group>
-
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </div>
-      <div className="graph">
-        <div>
-          <CanvasJSChart
-            options={options}
-            /* onRef={ref => this.chart = ref} */
-          />
-          {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+        <div className="graph">
+          <div>
+            <CanvasJSChart
+              options={options}
+              onRef={(ref) => (this.chart = ref)}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  componentDidMount() {
+    var chart = this.chart;
+    console.log(chart);
+    fetch("https://api.npoint.io/2f035e5c441af33d50a7")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        for (var i = 0; i < data.websites.amazon.length; i++) {
+          let time = data.datetime[i];
+          let ftime = new Date(time);
+          ftime =
+            String(ftime).slice(4, 10) + " " + String(ftime).slice(16, 21);
+          datapoint_amazon.push({
+            y: data.websites.amazon[i],
+            label: ftime,
+          });
+          datapoint_flipkart.push({
+            y: data.websites.flipkart[i],
+            label: ftime,
+          });
+        }
+        console.log(datapoint_amazon);
+        console.log(datapoint_flipkart);
+        chart.render();
+      });
+  }
 }
 
 export default Home;
